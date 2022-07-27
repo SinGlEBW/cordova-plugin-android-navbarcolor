0ce680948e1cb2a10c965cc0ddba5bca3e6d27d4
SinGlEBW <bw.klan@mail.ru>
2022-07-27 17:32:36 +0300

    update

--- AndroidKeyboard/src/android/AndroidKeyboard.java
+++ AndroidKeyboard/src/android/AndroidKeyboard.java
@@ -11,22 +11,125 @@ import org.json.JSONObject;
  * This class echoes a string called from JavaScript.
  */
 public class AndroidKeyboard extends CordovaPlugin {
+    /* Constructor */
+    public AndroidKeyboard() {
+    }
 
+    /*
+     * через execute предполагаем что будет вызываться через js и вызываем методы в
+     * java
+     */
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+            this.coolMethod(args, callbackContext);
             return true;
         }
+
+        if (action.equals("show")) {
+            /* callbackContext Если хотим передавать значения из java в js */
+            this.show(callbackContext);
+            return true;
+        }
+
+        if (action.equals("hide")) {
+            this.hide(callbackContext);
+            return true;
+        }
+
+        if (action.equals("getKeyboardHeight")) {
+            this.getKeyboardHeight(callbackContext);
+            return true;
+        }
+
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    /* Описываемые методы */
+    private void coolMethod(JSONArray args, CallbackContext callbackContext) {
+
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+        /*
+         * Получение аргументов. В зависимости от передаваемого типа выбираем что хотим
+         * тут получать
+         * int args1 = args.getInt(0);
+         * int args2 = args.getInt(1);
+         * String message = args.getString(0);
+         */
+    }
+
+    private void show(CallbackContext callbackContext) {
+        // if (message != null && message.length() > 0) {
+        // callbackContext.success(message);//
+        // } else {
+        // callbackContext.error("Expected one non-empty string argument.");
+        // }
+
+        this.cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // SYSTEM_UI_FLAG_FULLSCREEN is available since JellyBean, but we
+                // use KitKat here to be aligned with "Fullscreen" preference
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    int uiOptions = window.getDecorView().getSystemUiVisibility();
+                    uiOptions &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                    uiOptions &= ~View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+                    window.getDecorView().setSystemUiVisibility(uiOptions);
+                }
+
+                // CB-11197 We still need to update LayoutParams to force status bar
+                // to be hidden when entering e.g. text fields
+                window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        });
+    }
+
+    private void hide(CallbackContext callbackContext) {
+        /* Можно попробовать через callbackContext предположительно передавать event */
+        this.cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // SYSTEM_UI_FLAG_FULLSCREEN is available since JellyBean, but we
+                // use KitKat here to be aligned with "Fullscreen" preference
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    int uiOptions = window.getDecorView().getSystemUiVisibility()
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+                    window.getDecorView().setSystemUiVisibility(uiOptions);
+                }
+
+                // CB-11197 We still need to update LayoutParams to force status bar
+                // to be hidden when entering e.g. text fields
+                window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        });
+
+    }
+
+    public int getKeyboardHeight(CallbackContext callbackContext) {
+        final View rootview = this.getWindow().getDecorView();
+        linearChatLayout.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    public void onGlobalLayout() {
+                        Rect r = new Rect();
+                        rootview.getWindowVisibleDisplayFrame(r);
+                        int screenHeight = rootview.getRootView().getHeight();
+                        int newHeight = screenHeight - (r.bottom - r.top);
+                        if (newHeight > heightOfKeyboard) {
+                            heightOfKeyboard = screenHeight
+                                    - (r.bottom - r.top);
+                            // heightOfKeyboard = heightDiff;
+                        }
+
+                        Log.d("Keyboard Size", "Size: " + heightOfKeyboard);
+                    }
+                });
+        callbackContext.success(heightOfKeyboard);
     }
 }
--- AndroidKeyboard/www/AndroidKeyboard.js
+++ AndroidKeyboard/www/AndroidKeyboard.js
@@ -1,5 +1,24 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'AndroidKeyboard', 'coolMethod', [arg0]);
-};
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'AndroidKeyboard', 'coolMethod', [arg0]);
+// };
+
+let AndroidKeyboard = {
+    coolMethod: function (arg0, success, error) {
+        /* */
+        exec(success, error, 'AndroidKeyboard', 'coolMethod', [arg0]);
+    },
+
+    hide: function () {
+        exec(null, null, 'AndroidKeyboard', 'hide', []);
+    },
+
+    show: function () {
+        exec(null, null, 'AndroidKeyboard', 'show', []);
+    },
+    getKeyboardHeight: function (success) {
+        exec(success, null, 'AndroidKeyboard', 'getKeyboardHeight', []);
+    }
+}
+module.exports = AndroidKeyboard;
